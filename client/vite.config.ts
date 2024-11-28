3b53c4e623cb92af6ba96f1c9862d1ef331b95f2
jomasuleymen <joma.suleymen@gmail.com>
2024-11-28 16:17:20 +0500

    refactoring

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,23 @@
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from "vite-tsconfig-paths";
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [tsconfigPaths(), react()],
+	plugins: [
+		tsconfigPaths(),
+		react(),
+		viteCompression({
+			algorithm: "gzip",
+			deleteOriginFile: false,
+			compressionOptions: {
+				level: 5,
+			},
+			verbose: false,
+		}),
+	],
 	build: {
 		emptyOutDir: true,
 		outDir: "../server/client",
@@ -15,6 +27,13 @@ export default defineConfig({
 		alias: {
 			// for TypeScript path alias import like : @/x/y/z
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: "modern-compiler", // or "modern", "legacy"
+			},
 		},
 	},
 });
